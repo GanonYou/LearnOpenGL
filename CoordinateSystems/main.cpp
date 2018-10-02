c2ba458fa59e5d8220556cc5bf69397e832495cd
SwallowYWY <1169351969@qq.com>
2018-10-02 16:21:37 +0800

    坐标系统

--- CoordinateSystems/main.cpp
+++ CoordinateSystems/main.cpp
@@ -31,7 +31,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello,matrix", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "CoordinateSystems", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -48,17 +48,68 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    
+    //启用深度测试，需要开启GL_DEPTH_TEST
+    glEnable(GL_DEPTH_TEST);
 
     //创建并编译我们的着色器
-    Shader ourShader("/Users/YWY/XcodeProjects/LearnOpenGL/Matrix/my_shaders/shader_textures.vs", "/Users/YWY/XcodeProjects/LearnOpenGL/Matrix/my_shaders/shader_textures.fs");
+    Shader ourShader("/Users/YWY/XcodeProjects/LearnOpenGL/CoordinateSystems/my_shaders/shader_textures.vs", "/Users/YWY/XcodeProjects/LearnOpenGL/CoordinateSystems/my_shaders/shader_textures.fs");
 
     /*************** 设置顶点数据与缓冲，并配置顶点属性 *************/
     float vertices[] = {
-        // positions          // texture coords
-        0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
-        0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
     unsigned int indices[] = {
         0, 1, 3, // first triangle
@@ -100,7 +151,7 @@ int main()
 
     //加载图像，创建纹理，生成mipmap
     int width, height, nrChannels;
-    std::string path = "/Users/YWY/XcodeProjects/LearnOpenGL/Matrix/container.jpg";
+    std::string path = "/Users/YWY/XcodeProjects/LearnOpenGL/CoordinateSystems/container.jpg";
     unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -126,7 +177,7 @@ int main()
 
     //在图像加载时帮助我们翻转y轴，只需要在加载任何图像前加入以下语句即可
     stbi_set_flip_vertically_on_load(true);
-    path = "/Users/YWY/XcodeProjects/LearnOpenGL/Matrix/awesomeface.png";
+    path = "/Users/YWY/XcodeProjects/LearnOpenGL/CoordinateSystems/pipi.png";
     data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -151,7 +202,8 @@ int main()
 
         //渲染
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        //在每次渲染迭代之前清除深度缓冲（否则前一帧的深度信息仍然保存在缓冲中）
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //在绑定纹理之前先激活纹理单元
         glActiveTexture(GL_TEXTURE0);
@@ -168,22 +220,37 @@ int main()
         transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
         //设置Uniform
         ourShader.use();
-        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-        //Matrix4fv函数把矩阵数据发送给着色器。参数依次是uniform的位置值、矩阵个数，是否希望置换以及矩阵数据
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-        //渲染容器
+        
+        //创建变换
+        glm::mat4 view;
+        glm::mat4 projection;
+        //模型矩阵，将坐标从局部坐标映射到世界空间
+        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        //观察矩阵，将坐标从世界空间映射到观察(摄像机)空间
+        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        //投影矩阵，将坐标从观察空间映射到裁剪空间
+        //函数第一个参数是fov(Field of View),第二个是宽高比，第三和第四个参数设置了平截头体的近和远平面
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        //设置多个uniform
+        ourShader.setMat4("view", view);
+        ourShader.setMat4("projection", projection);
+        
+        //渲染容器，并渲染10个
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
-        //渲染第二个，记得初始化transform
-        transform = glm::mat4();
-        transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
-        float scaleAmount = sin(glfwGetTime());
-        transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount)); //缩放
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]);
-        //渲染容器
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model;
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            
+            if (i % 3 == 0)
+                angle = glfwGetTime() * 25.0f;
+            
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+            
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         //交换缓冲并查询IO事件
         glfwSwapBuffers(window);
---
+++ CoordinateSystems/new_for_stb.cpp
@@ -0,0 +1,10 @@
+//
+//  new_for_stb.cpp
+//  Textures
+//
+//  Created by 游文彦 on 2018/9/26.
+//  Copyright © 2018 Swallow. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
